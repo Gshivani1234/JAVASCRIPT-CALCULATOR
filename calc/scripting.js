0d84412a1880d94b5cc9ae95db83082d32d741ec
Gshivani1234 <40537358+Gshivani1234@users.noreply.github.com>
2018-09-06 14:56:54 +0530

    CALCULATOR  USING HTML CSS JAVASCRIPT

---
+++ calc/scripting.js
@@ -0,0 +1,119 @@
+(function(){
+	"use strict";
+
+	var ele = function(element)
+	{
+		if (element.charAt(0)==="#")
+		{
+			return document.queryselector(element);
+		}
+		return document.queryselectorAll(element);
+	};
+
+	//variables
+	var viewer=ele("#viewer"),
+	equals=ele("#equals"),
+	nums=ele("#nums"),
+	ops=ele("#ops"),
+	newnum="",
+	oldnum="",
+	resultnum="",
+	operator="";
+
+	//when number is clicked.
+	var setnum=function()
+	{
+		if (resultnum) {
+			newnum=this.getAttribute("data-num");
+			resultnum="";
+		}
+		else
+		{
+			newnum=newnum+this.getAttribute("data-num");
+		}
+		viewer.innerHtml=newnum;	
+	};
+
+	//when operator is clicked
+	var movenum=function()
+	{
+		oldnum=newnum;
+		newnum="";
+		operator=this.getAttribute("data-ops");
+
+		equals.setAttribute("data-result","");
+	};
+
+	//calculate result when equals is clicked
+	var displaynum=function()
+	{
+
+		oldnum=parseFloat(oldnum);
+		newnum=parseFloat(newnum);
+
+		switch(operator)
+		{
+			case 'plus':
+			resultnum=oldnum+newnum;
+			break;
+
+			case 'minus':
+			resultnum=oldnum-newnum;
+			break;
+
+			case 'multiply':
+			resultnum=oldnum*newnum;
+			break;
+
+			case 'divide':
+			resultnum=oldnum/newnum;
+			break;
+
+			default
+			resultnum=newnum;
+		}
+		if(!isFinite(resultnum))
+		{
+			if (isNaN(resultnum)) {
+
+				resultnum="NOT A NUMBER";
+			}
+			else
+			{
+				resultnum="INVALID OUTPUT-READ THE WARNING"
+			}	
+		}
+
+		//Display result
+		viewer.innerHtml=resultnum;
+		equals.setAttribute("data-result",resultnum);
+
+		oldnum=0;
+		newnum=resultnum;
+
+		//clear all
+		var clearall=function(){
+			oldnum="";
+			newnum="";
+			viewer,innerHtml=0;
+			equals.setAttribute("data-result",resultnum);
+		};
+
+	//ADD CLICK EVENTS
+
+	//for numbers
+	for(i=0,l=nums.length();i<l;i++)
+		nums[i]=onclick = setnum;
+    
+
+    //for operators
+    for(i=0,l=ops.length();i<l;i++)
+		ops[i]=onclick = movenum;
+    
+    //for equals sign
+    equals.onclick=displaynum;
+
+    //for clear
+    ele("#clear").onclick=clearall;
+}
+}());
